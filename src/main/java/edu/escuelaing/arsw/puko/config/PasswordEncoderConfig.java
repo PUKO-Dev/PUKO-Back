49ede3aa933f32f278447b4aa794fbd9ff6f2d0f
juli <bricenojuliana@gmail.com>
2024-11-26 22:17:22 -0500

    SonarCloud?? AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA

--- src/main/java/edu/escuelaing/arsw/puko/config/PasswordEncoderConfig.java
+++ src/main/java/edu/escuelaing/arsw/puko/config/PasswordEncoderConfig.java
@@ -8,7 +8,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 @Configuration
 public class PasswordEncoderConfig  {
 
-    private PasswordEncoderConfig() {
+    public PasswordEncoderConfig() {
     }
 
     @Bean
