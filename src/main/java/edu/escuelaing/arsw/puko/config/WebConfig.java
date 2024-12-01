f05c1506edb34fcbff43b9f79bb43e21a7c511d3
CristianAlvarez-b <cristian.alvarez-b@mail.escuelaing.edu.co>
2024-12-01 00:51:00 -0500

    Authorizando ip balanceador

--- src/main/java/edu/escuelaing/arsw/puko/config/WebConfig.java
+++ src/main/java/edu/escuelaing/arsw/puko/config/WebConfig.java
@@ -12,11 +12,12 @@ public class WebConfig implements WebMvcConfigurer {
     public WebMvcConfigurer corsConfigurer() {
         String vs = "http://127.0.0.1:5500";
         String front = "https://thankful-sea-00a591a10.5.azurestaticapps.net";
+        String balancer = "http://135.233.99.171";
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins(front, vs) // Cambia esto al dominio del frontend
+                        .allowedOrigins(front, vs, balancer) // Cambia esto al dominio del frontend
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
